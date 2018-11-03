3da777dd3a79444c9f1ea403692493ebdc4bfb9f
shashi shanker singh <shankar634@hotmail.com>
2018-11-03 09:11:57 +0530

    Unity Dependency Injection Package is added

---
+++ Examo/App_Start/UnityDependencyResolver.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Unity;
+
+namespace Examo
+{
+    internal class UnityDependencyResolver : IDependencyResolver
+    {
+        private UnityContainer container;
+
+        public UnityDependencyResolver(UnityContainer container)
+        {
+            this.container = container;
+        }
+    }
+}
